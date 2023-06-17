320eb23f7b55690cbb143cba02b47b2d04817867
Leonel <juizleonel@gmail.com>
2023-06-17 11:01:39 -0300

    Clase 2: Creadas etiquetas y a comenzar a crear los campos

--- CRUD 23208/ejemplo_tkinter.py
+++ CRUD 23208/ejemplo_tkinter.py
@@ -1,5 +1,9 @@
 from tkinter import *
 
+#Framecampos
+color_fondo = 'cyan'
+color_letra = 'black'
+
 # RAÍZ
 raiz = Tk()
 raiz.title('GUI - Comisión 23208')
@@ -34,6 +38,55 @@ barramenu.add_cascade(label = 'Gráficas', menu=estadmenu)
 barramenu.add_cascade(label = 'Limpiar', menu=limpiarmenu)
 barramenu.add_cascade(label = 'Acerca de...', menu=ayudamenu)
 
+# ----- FRAME CAMPOS -----
+framecampos = Frame(raiz)
+framecampos.config(bg=color_fondo)
+framecampos.pack(fill='both')
+
+#Labels
+def config_label(mi_label, fila):
+    espaciado_labels = {'column':0, 'sticky':'e', 'padx':10, 'pady':10}
+    color_labels = {'bg':color_fondo, 'fg':color_letra}
+    mi_label.grid(row=fila, **espaciado_labels)
+    mi_label.config(**color_labels)
+
+legajo_label = Label(framecampos, text='N° de legajo')
+config_label(legajo_label,0)
+
+apellido_label = Label(framecampos, text='Apellido')
+config_label(apellido_label,1)
+
+nombre_label = Label(framecampos, text='Nombre')
+config_label(nombre_label,2)
+
+email_label = Label(framecampos, text='E-mail')
+config_label(email_label,3)
+
+promedio_label = Label(framecampos, text='Promedio')
+config_label(promedio_label,4)
+
+escuela_label = Label(framecampos, text='Escuela')
+config_label(escuela_label,5)
+
+localidad_label = Label(framecampos, text='Localidad')
+config_label(localidad_label,6)
+
+provincia_label = Label(framecampos, text='Provincia')
+config_label(provincia_label,7)
+
+# ENTRY
+# Crear variables de control de los campos de entrada
+legajo = StringVar()
+apellido = StringVar()
+nombre = StringVar()
+email = StringVar()
+calificacion = DoubleVar()
+escuela = StringVar()
+localidad = StringVar()
+provincia = StringVar()
 
 
-raiz.mainloop() 
+
+legajo_input
+
+raiz.mainloop()
